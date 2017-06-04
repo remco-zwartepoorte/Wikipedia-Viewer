0bf587f5579c8c73c2ee691dc12bfc75d0a830ae
Remco Zwartepoorte <remco.zwartepoorte@gmail.com>
2017-06-04 10:21:32 +0200

    Display search results

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
 });
 
-document.getElementById("go-button").addEventListener('click', saveSearch);
+document.getElementById("go-button").addEventListener('click', saveSearchText);
 
-var searchText;
+var searchText, dataArray, resultList;
 
-function saveSearch() {
+function saveSearchText() {
   searchText = document.getElementById("searchtext").value;
-  getSearch();
+  fetchSearchResults();
 }
 
-function getSearch() {
+function fetchSearchResults() {
   var fetchURL = "https://en.wikipedia.org/w/api.php?&origin=*&action=opensearch&search=" + searchText + "&limit=10";
   fetch(fetchURL, {
     cache: "reload"
@@ -22,11 +22,21 @@ function getSearch() {
         return;
       }
       response.json().then(function (data) {
+        dataArray = data;
         console.log(data);
+        displaySearchResults();
       });
     })
     .catch(function (err) {
       console.log('Fetch Error :-S', err);
     });
 
+}
+
+function displaySearchResults() {
+  resultList = "";
+  for (var i = 0; i < dataArray[1].length; i++) {
+    resultList += "<a " + "href=" + dataArray[3][i] + "><dt>" + dataArray[1][i] + "</dt></a>" + "<dd>" + dataArray[2][i] + "<dd>";
+  }
+  document.getElementById("searchresults").innerHTML = resultList;
 }

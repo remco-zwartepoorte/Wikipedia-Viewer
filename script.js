ccda3c268c080a56e17de9a90e981b9d6826263d
Remco Zwartepoorte <remco.zwartepoorte@gmail.com>
2017-06-04 13:29:44 +0200

    added # search results found

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-document.addEventListener('DOMContentLoaded', function () {
-});
+//document.addEventListener('DOMContentLoaded', function () {
+//});
 
 document.getElementById("go-button").addEventListener('click', saveSearchText);
+document.addEventListener('keypress', function (event) {
+  if (event.keyCode == 13) {
+    document.getElementById("go-button").click();
+  }
+});
 
 var searchText, dataArray, resultList;
 
@@ -37,10 +42,22 @@ function displaySearchResults() {
   resultList = "";
   if (dataArray[1].length == 0) {
     resultList = "<p>Sorry, no results found</p>";
+    makeList();
+  } else if (dataArray[1].length == 1) {
+    resultList = "<p>1 result is found:</p >";
+    makeList();
+  } else if (dataArray[1].length != 0 && dataArray[1].length != 1 && dataArray[1].length < 10) {
+    resultList = "<p> " + dataArray[1].length + " results are found:</p>";
+    makeList();
+  } else {
+    resultList = "<p>Top 10 results are:</p >";
+    makeList();
   }
-  for (var i = 0; i < dataArray[1].length; i++) {
-    resultList += "<a " + "href=" + dataArray[3][i] + "><dt>" + dataArray[1][i] + "</dt></a>" + "<dd>" + dataArray[2][i] + "<dd>";
-  }
+}
 
+function makeList() {
+  for (var i = 0; i < dataArray[1].length; i++) {
+    resultList += "<a " + "href=" + dataArray[3][i] + "><dt>" + dataArray[1][i] + "</dt></a>" + "<dd>" + dataArray[2][i] + "</dd>";
+  }
   document.getElementById("searchresults").innerHTML = resultList;
 }
